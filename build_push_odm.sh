8fc9d6fb304de6685a85c582f9921eddef1b907c
liang.wang <liang.wang@ck-telecom.com>
2014-05-29 19:35:52 +0800

    continue
    [Solution Description]
    modified:   build_push_odm.sh
    [Other info]
    N/A

--- build_push_odm.sh
+++ build_push_odm.sh
@@ -1,11 +1,28 @@
-#! /bin/sh
+#! /Bin/sh
+###########################################################
+# this script is used for build HiCamera apk for different
+# branch
+#
+#
+############################################################
+echo "###########################################################
+ this script is used for build HiCamera apk for different branch
+###########################################################"
+
+CameraWorkingBranch="m4_ss4020"
+CameraPath="$HOME/work/HiCamera3.0"
+ProjectPath="/mnt/sdc1/sz_SS4020"
+
 #printf "%s\n" "$HOME"
-CMD="cd $HOME/work/HiCamera3.0"
+CMD="cd $CameraPath"
 #'A Practice guide to linux' 8.3.1 said, '',"",is different
 #CMD='cd $HOME/work/HiCamera3.0' # wrong
 #CMD="'cd' $HOME'/work/HiCamera3.0'" # wrong
 echo $CMD
 $CMD
-CMD="git branch |grep \*"
+CMD="git rev-parse --abbrev-ref HEAD"
 echo $CMD
 HiCameraBranch=`$CMD`
+echo $HiCameraBranch
+
+
