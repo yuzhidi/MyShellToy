83bce9685cb8dfd69cd562281e5c0fefd9fa090b
liang.wang <liang.wang@ck-telecom.com>
2014-05-30 16:51:00 +0800

    [][][][print hiCamera3.0 branch and project branch]
    [Solution Description]
    modified:   build_push_odm.sh
    [Other info]
    N/A

--- build_push_odm.sh
+++ build_push_odm.sh
@@ -1,18 +1,20 @@
-#! /Bin/sh
+#! /bin/sh
 ###########################################################
 # this script is used for build HiCamera apk for different
 # branch
 #
 #
 ############################################################
-echo "###########################################################
- this script is used for build HiCamera apk for different branch
-###########################################################"
 
+GITCB="git rev-parse --abbrev-ref HEAD"
 CameraWorkingBranch="m4_ss4020"
 CameraPath="$HOME/work/HiCamera3.0"
 ProjectPath="/mnt/sdc1/sz_SS4020"
-
+ProjectBranch=
+echo -e "\033[33m###########################################################
+ check HiCamera3.0 branch
+###########################################################"
+echo -ne "\033[0m";
 #printf "%s\n" "$HOME"
 CMD="cd $CameraPath"
 #'A Practice guide to linux' 8.3.1 said, '',"",is different
@@ -20,9 +22,17 @@ CMD="cd $CameraPath"
 #CMD="'cd' $HOME'/work/HiCamera3.0'" # wrong
 echo $CMD
 $CMD
-CMD="git rev-parse --abbrev-ref HEAD"
-echo $CMD
-HiCameraBranch=`$CMD`
+
+HiCameraBranch=`$GITCB`
 echo $HiCameraBranch
 
+echo -e "\033[33m###########################################################
+ inter project
+###########################################################"
+echo -ne "\033[0m";
 
+cd $ProjectPath
+PWD=`pwd`
+echo $PWD
+ProjectPath=`$GITCB`
+echo $ProjectPath
